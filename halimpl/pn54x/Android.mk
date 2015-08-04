7e1dd4ad936ca35af4b4df66f8628c5ee3f0bc38
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2015-08-04 16:52:50 +0200

    Revert "Merge pull request #3 from AdFad666/nfc-hal"
    This reverts commit 79fc17f156d3302bed80244e82b0f5df06b4ccf3, reversing
    changes made to ddf0efda9cf7faf63f7ca7f3e7eb82d8bd09b26a.

--- halimpl/pn54x/Android.mk
+++ halimpl/pn54x/Android.mk
@@ -31,7 +31,7 @@ LOCAL_PRELINK_MODULE := false
 LOCAL_ARM_MODE := arm
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE := nfc_nci.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := nfc_nci.pn54x.default
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SRC_FILES := $(call all-c-files-under, .)  $(call all-cpp-files-under, .)
 LOCAL_SHARED_LIBRARIES := liblog libcutils libhardware_legacy libdl libstlport
